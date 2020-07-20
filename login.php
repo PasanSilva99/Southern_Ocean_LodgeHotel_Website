36a0f589e74da57aa42d7e7379b2d3f71c8ff695
Pasan Anuththara <pasananuththara19@gmail.com>
2020-07-20 19:19:31 +0530

    Change the theme bit
    Chack whether its ok

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $password = "";
 $dbName = "hoteldb";
 $userPassword;
 $userUserName;
-$retrievedUserName = $_REQUEST["UserName"];
+$retrievedUserName = $_REQUEST["email"];
 $retrievedPassword = $_REQUEST["psw"];
 
 $con = new mysqli($servername, $username, $password, $dbName);
@@ -15,24 +15,23 @@ if($con->connect_error){
     die("Connection Failed: ". $con->connect_error);
 }
 
-$qry = "SELECT UserName, Password FROM UserData";
+$qry = "SELECT Email, Password FROM UserData";
 $result = $con->query($qry);
 
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
-    $userUserName = $row["UserName"];
+    $userUserName = $row["Email"];
     $userPassword = $row["Password"];
     }
 }
 
-if($retrievedPassword == $userPassword && $retrievedUserName == $userUserName)
-{
-    echo "Bravo... I got your Username Password. I will hack your account now!<br />";
+if($retrievedPassword == $userPassword && $retrievedUserName == $userUserName){
+    #Password Works
+    header("Location: index.html");
 }
-else
-{
-    echo "Hola! How are you?<br />";
-}
+else{
+    #Login Unsuccessfull
 
+}
 
 ?>
