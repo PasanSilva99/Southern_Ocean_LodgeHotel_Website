aba8eff9d8bbd969adf845427468c6a0dfa851e5
Pasan Anuththara <pasananuththara19@gmail.com>
2020-07-20 18:33:38 +0530

    Created Login

--- login.php
+++ login.php
@@ -1,14 +1,38 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<body>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "hoteldb";
+$userPassword;
+$userUserName;
+$retrievedUserName = $_REQUEST["UserName"];
+$retrievedPassword = $_REQUEST["psw"];
 
-    <h1>My first PHP page</h1>
+$con = new mysqli($servername, $username, $password, $dbName);
 
-    <?php
-echo "Hello World!";
+if($con->connect_error){
+    die("Connection Failed: ". $con->connect_error);
+}
+
+$qry = "SELECT UserName, Password FROM UserData";
+$result = $con->query($qry);
+
+if($result->num_rows>0){
+    while($row = $result->fetch_assoc()){
+    $userUserName = $row["UserName"];
+    $userPassword = $row["Password"];
+    }
+}
+
+if($retrievedPassword == $userPassword && $retrievedUserName == $userUserName)
+{
+    echo "Bravo... I got your Username Password. I will hack your account now!<br />";
+}
+else
+{
+    echo "Hola! How are you?<br />";
+}
+
+
 ?>
-
-</body>
-
-</html>
