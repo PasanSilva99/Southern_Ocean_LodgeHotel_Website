bf656a6a50eeb7a035010d81e62793ed666a6c4e
Pasan Anuththara <pasananuththara19@gmail.com>
2020-07-22 11:01:55 +0530

    Signup Finished

--- signup.php
+++ signup.php
@@ -3,4 +3,33 @@
     $LastName = $_REQUEST["lastName"];
     $Email = $_REQUEST["email"];
     $psw = $_REQUEST["psw"];    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbName = "hoteldb";
+
+    $con = new mysqli($servername, $username, $password, $dbName);
+
+    if($con->connect_error){
+        die("Connection Failed: ". $con->connect_error);
+    }
+    
+    $qry = "INSERT INTO userData (FirstName, LastName, Email, Password) VALUES('".$FirstName ."', '".$LastName."', '".$Email."', '".$psw."')";
+    $result = $con->query($qry);
+    
+    if ($con->query($qry) === TRUE) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $qry . "<br>" . $con->error;
+      }
+    
+    if($retrievedPassword == $userPassword && $retrievedUserName == $userUserName){
+        #Password Works
+        header("Location: index.html");
+    }
+    else{
+        #Login Unsuccessfull
+    
+    }
+    
 ?>
